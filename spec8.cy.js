85bfc42684f88656eb82879c255e9bd28fb0255b
Gabriel Soares Marcondes <143465314+Gabriel-S-M@users.noreply.github.com>
2025-06-05 21:18:15 -0300

    Update spec8.cy.js

--- spec8.cy.js
+++ spec8.cy.js
@@ -1,7 +1,7 @@
 describe('Teste - revisão', () => {
 
   beforeEach(() => {
-    cy.visit('http://localhost:8000');
+    cy.visit('http://127.0.0.1:5500/index.html');
   });
 
   it('Login com Espera Explícita', () => {
